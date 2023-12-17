9f40ff69c1ab0d5fd44c00614e8e833157d83a1f
DSH <dupecube1@gmail.com>
2023-12-17 17:47:10 +0400

    Server Create: added server create form with image upload, imageupload package name: uploadthing

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import {db} from "@/lib/db";
 import {initialProfile} from "@/lib/initial-profile";
 import {redirect} from "next/navigation";
+import {InitialModal} from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
     const profile = await initialProfile();
@@ -20,9 +21,7 @@ const SetupPage = async () => {
         return redirect(`/servers/${server.id}`)
     }
 
-    return (
-        <div>Create a Server</div>
-    )
+    return <InitialModal/>
 }
 
 export default SetupPage;
