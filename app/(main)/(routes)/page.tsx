a3bdbf89dd2dc643e1377378fab9c2a43a8091be
DSH <dupecube1@gmail.com>
2023-12-17 13:50:41 +0400

    DarkMode: implement shadcn dark-theme

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
 import {UserButton} from "@clerk/nextjs";
+import {ModeToggle} from "@/components/mode-toggle";
 
 export default function Home() {
     return (
         <div className="text-gray-500">
             <UserButton afterSignOutUrl="/"/>
+            <ModeToggle/>
         </div>
     )
 }
