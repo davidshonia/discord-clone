1d291217558229cd664557e9858427ac95dd5db5
DSH <dupecube1@gmail.com>
2023-12-17 18:13:15 +0400

    Server Creating Api: api for server creating with database insert is currently working

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -27,6 +27,8 @@ import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 import {useEffect, useState} from "react";
 import {FileUpload} from "@/components/file-upload";
+import axios from "axios";
+import { useRouter } from 'next/navigation';
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -38,6 +40,8 @@ const formSchema = z.object({
 })
 
 export const InitialModal = () => {
+
+    const router = useRouter();
 
     const [isMounted, setIsMounted] = useState(false);
 
@@ -56,7 +60,15 @@ export const InitialModal = () => {
     const isLoading = form.formState.isSubmitting;
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values)
+        try {
+            await axios.post("api/servers", values);
+            form.reset()
+            router.refresh();
+            window.location.reload()
+        } catch (error)
+        {
+            console.log(error)
+        }
     }
 
     if (!isMounted) {
